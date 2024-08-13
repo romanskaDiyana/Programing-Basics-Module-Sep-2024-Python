5cf427a4762abb850d207e647a33b20c052f828b
romanskadiyana <romanskadiyana@gmail.com>
2024-08-13 16:06:23 +0300

    conditional_statements_LAB

---
+++ conditional_statements_LAB/02_greater_number.py
@@ -0,0 +1,8 @@
+# Да се напише програма, която чете две цели числа въведени от потребителя и отпечатва по-голямото от двете.
+
+number1 = int(input())
+number2 = int(input())
+
+if number1 > number2:
+    print(number1)
+else: print(number2)
